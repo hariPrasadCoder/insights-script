598c64d95088adc5517e5a194901c407573c6e5c
HARI PRASAD <65354242+hariPrasadCoder@users.noreply.github.com>
2025-04-03 15:35:54 -0400

    Update insights.js

--- insights.js
+++ insights.js
@@ -1,35 +1,26 @@
-(function () {
-  const script = document.createElement("script");
-  script.src = "https://app.posthog.com/static/array.js";
-  document.head.appendChild(script);
+<script>
+  !function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.crossOrigin="anonymous",p.async=!0,p.src=s.api_host.replace(".i.posthog.com","-assets.i.posthog.com")+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="init capture register register_once register_for_session unregister unregister_for_session getFeatureFlag getFeatureFlagPayload isFeatureEnabled reloadFeatureFlags updateEarlyAccessFeatureEnrollment getEarlyAccessFeatures on onFeatureFlags onSurveysLoaded onSessionId getSurveys getActiveMatchingSurveys renderSurvey canRenderSurvey identify setPersonProperties group resetGroups setPersonPropertiesForFlags resetGroupPropertiesForFlags reset get_distinct_id getGroups get_session_id get_session_replay_url alias set_config startSessionRecording stopSessionRecording sessionRecordingStarted captureException loadToolbar get_property getSessionProperty createPersonProfile opt_in_capturing opt_out_capturing has_opted_in_capturing has_opted_out_capturing clear_opt_in_out_capturing debug getPageViewId captureTraceFeedback captureTraceMetric".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
+  
+  const clientId = document.currentScript.getAttribute("data-project");
+  posthog.init('phc_kLiYSRLtrulVfpgAXUATSD0uqJ3kgidQ6pbfG9jMCcd', {
+    api_host: 'https://us.i.posthog.com',
+    person_profiles: 'identified_only',
+    loaded: function(ph) {
+      ph.identify(clientId);
 
-  script.onload = function () {
-    window.posthog = window.posthog || [];
-    const clientId = document.currentScript.getAttribute("data-project");
-
-    window.posthog.init("phc_kLiYSRLtrulVfpgAXUATSD0uqJ3kgidQ6pbfG9jMCcd", {
-      api_host: "https://app.posthog.com",
-      loaded: function (ph) {
-        ph.identify(clientId);
-      },
-    });
-
-    // Track CTA clicks
-    document.querySelectorAll("[data-cta]").forEach((btn) => {
-      btn.addEventListener("click", () => {
-        posthog.capture("cta_clicked", {
-          label: btn.innerText,
+      // ✅ Your custom events
+      document.querySelectorAll("[data-cta]").forEach(btn => {
+        btn.addEventListener("click", () => {
+          posthog.capture("cta_clicked", { label: btn.innerText });
         });
       });
-    });
 
-    // Scroll tracking
-    window.addEventListener("scroll", function () {
-      let scrollPercent =
-        (window.scrollY + window.innerHeight) / document.body.scrollHeight;
-      if (scrollPercent > 0.75) {
-        posthog.capture("scroll_75_percent");
-      }
-    });
-  };
-})();
+      window.addEventListener("scroll", function () {
+        const scrollPercent = (window.scrollY + window.innerHeight) / document.body.scrollHeight;
+        if (scrollPercent > 0.75) {
+          posthog.capture("scroll_75_percent");
+        }
+      });
+    }
+  });
+</script>
