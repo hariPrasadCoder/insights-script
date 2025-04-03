08ce094eb8264aa3500415369e38549b9f7a22c2
HARI PRASAD <65354242+hariPrasadCoder@users.noreply.github.com>
2025-04-03 14:27:15 -0400

    Create insights.js

---
+++ insights.js
@@ -0,0 +1,35 @@
+(function () {
+  const script = document.createElement("script");
+  script.src = "https://app.posthog.com/static/array.js";
+  document.head.appendChild(script);
+
+  script.onload = function () {
+    window.posthog = window.posthog || [];
+    const clientId = document.currentScript.getAttribute("data-project");
+
+    window.posthog.init("phx_ZrLHCdhQ5jo0SKx4PRrqJjaZmorUeYojWlO2HhgSptL82pW", {
+      api_host: "https://app.posthog.com",
+      loaded: function (ph) {
+        ph.identify(clientId);
+      },
+    });
+
+    // Track CTA clicks
+    document.querySelectorAll("[data-cta]").forEach((btn) => {
+      btn.addEventListener("click", () => {
+        posthog.capture("cta_clicked", {
+          label: btn.innerText,
+        });
+      });
+    });
+
+    // Scroll tracking
+    window.addEventListener("scroll", function () {
+      let scrollPercent =
+        (window.scrollY + window.innerHeight) / document.body.scrollHeight;
+      if (scrollPercent > 0.75) {
+        posthog.capture("scroll_75_percent");
+      }
+    });
+  };
+})();
